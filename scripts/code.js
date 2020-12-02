7c86e4c4b8e5efe194c296ab437ac9169a10d091
Daniel <danielflores1470@gmail.com>
2020-12-02 15:32:21 -0600

    se agrego el buscador beta a el molde

---
+++ scripts/code.js
@@ -0,0 +1,39 @@
+const d = document;
+const buscador = d.querySelector(".buscador");
+const boton_colapso = d.querySelector(".p2");
+const boton_2 = d.querySelector(".n2");
+const boton_1 = d.querySelector(".n1");
+
+d.addEventListener("click",e => {
+    if(e.target.matches(".arrow .n1")){
+        e.target.classList.add("invisible");
+        e.target.classList.add("eliminado");
+
+        buscador.classList.add("lucecita");
+
+        setTimeout(() => {
+            buscador.classList.remove("lucecita");
+            buscador.classList.add("colapso");
+            boton_colapso.classList.add("colapso-bt");
+        }, 500);
+
+        setTimeout(() => {
+            boton_2.classList.remove("eliminado");
+            boton_2.classList.remove("invisible");
+        },700)
+
+    }
+
+    if(e.target.matches(".n2")){
+        e.target.classList.add("invisible");
+        e.target.classList.add("eliminado");
+
+        buscador.classList.remove("colapso");
+        boton_colapso.classList.remove("colapso-bt");
+
+        setTimeout(() => {
+            boton_1.classList.remove("eliminado");
+            boton_1.classList.remove("invisible");
+        },400)
+    }
+})
